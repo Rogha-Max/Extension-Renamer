4ca803089841ef1cd05fa999ea0d10290c54f23b
Rogha-Max <maxime.desgranges1@gmail.com>
2022-02-10 20:35:33 +0100

    Choice of the starting and ending extension

--- convert.bat
+++ convert.bat
@@ -1,2 +1,11 @@
-@echo off
-ren *.txt *.js
+echo off
+title Extension renamer
+cls
+echo ******************************
+echo ***** Extension renamer ******
+echo ******************************
+set /p From=Convertir les .
+set /p To=En .
+ren *.%From% *.%To%
+echo DONE.
+pause
